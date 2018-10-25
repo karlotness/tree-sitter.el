3d3db6f37cf70f06f9166b1395eee889433caed2
Karl Otness <karl@karlotness.com>
2018-10-25 22:14:41 +0100

    Add missing provides call for tree-sitter languages.

--- utils/language/tree-sitter-lang-LANG_NAME.el
+++ utils/language/tree-sitter-lang-LANG_NAME.el
@@ -1,3 +1,5 @@
 ;;; tree-sitter.el --- Emacs bindings to the tree-sitter parser for LANG_NAME  -*- lexical-binding: t; -*-
 
 (require 'tree-sitter-lang-LANG_NAME-module)
+
+(provide 'tree-sitter-lang-LANG_NAME)
