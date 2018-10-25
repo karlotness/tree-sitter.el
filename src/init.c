18f4252f0a14f08939c5f4114aea238fdf3f78c3
Karl Otness <karl@karlotness.com>
2018-10-25 20:13:56 +0100

    Add skeleton for TSTree type and functions.

--- src/init.c
+++ src/init.c
@@ -22,6 +22,7 @@
 #include "language.h"
 #include "symbol.h"
 #include "parser.h"
+#include "tree.h"
 
 // Required symbol for Emacs loading
 int plugin_is_GPL_compatible;
@@ -39,7 +40,8 @@ int emacs_module_init(struct emacs_runtime *ert) {
   }
   // Perform initialization
   if(!tsel_common_init(env) || !tsel_language_init(env) ||
-     !tsel_symbol_init(env) || !tsel_parser_init(env)) {
+     !tsel_symbol_init(env) || !tsel_parser_init(env) ||
+     !tsel_tree_init(env)) {
     return 1;
   }
   // Provide the module
---
+++ src/tree.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2018 Karl Otness
+ *
+ * This file is part of tree-sitter.el.
+ *
+ * tree-sitter.el is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * tree-sitter.el is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tree-sitter.el. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+#include "tree.h"
+
+bool tsel_tree_init(emacs_env *env) {
+  return true;
+}
