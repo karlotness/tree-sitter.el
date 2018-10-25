36d29e7619a73f6e0c6b5a741905b6011a05b7d0
Karl Otness <karl@karlotness.com>
2018-10-25 10:26:38 +0100

    Update symbol to new raw record.

--- src/symbol.c
+++ src/symbol.c
@@ -20,29 +20,45 @@
 #include "symbol.h"
 #include "common.h"
 
+char *tsel_symbol_p_wrapped_doc = "Return t if OBJECT is a tree-sitter-symbol.\n"
+  "\n"
+  "(fn OBJECT)";
+static emacs_value tsel_symbol_p_wrapped(emacs_env *env,
+                                         __attribute__((unused)) ptrdiff_t nargs,
+                                         emacs_value *args,
+                                         __attribute__((unused)) void *data) {
+  if(tsel_symbol_p(env, args[0])) {
+    return tsel_Qt;
+  }
+  return tsel_Qnil;
+}
+
 bool tsel_symbol_init(__attribute__((unused)) emacs_env *env) {
+  bool function_result = tsel_define_function(env, "tree-sitter-symbol-p",
+                                              &tsel_symbol_p_wrapped, 1, 1,
+                                              tsel_symbol_p_wrapped_doc, NULL);
+  if(!function_result) {
+    return false;
+  }
   return true;
 }
 
 bool tsel_symbol_p(emacs_env *env, emacs_value obj) {
-  // Ensure the pointer is wrapped in the proper Emacs structure type
-  emacs_value Qts_lang_p = env->intern(env, "tree-sitter-symbol-p");
-  emacs_value args[1] = { obj };
-  if(!env->eq(env, env->funcall(env, Qts_lang_p, 1, args), tsel_Qt) ||
-     tsel_pending_nonlocal_exit(env)) {
+  if(!tsel_check_record_type(env, "tree-sitter-symbol", obj)) {
     return false;
   }
-  // Extract the "code" field and make sure it's an integer
-  emacs_value Qts_symbol_code = env->intern(env, "tree-sitter-symbol-code");
-  emacs_value code = env->funcall(env, Qts_symbol_code, 1, args);
-  emacs_value Qintegerp = env->intern(env, "integerp");
-  args[0] = code;
-  if(!env->eq(env, env->funcall(env, Qintegerp, 1, args), tsel_Qt) ||
-     tsel_pending_nonlocal_exit(env)) {
+  // Get the code field
+  emacs_value code;
+  if(!tsel_record_get_field(env, obj, 1, &code)) {
     return false;
   }
+  // Ensure the code is non-negative
   intmax_t raw_code = env->extract_integer(env, code);
-  return raw_code >= 0;
+  if(raw_code < 0 || tsel_pending_nonlocal_exit(env)){
+    env->non_local_exit_clear(env);
+    return false;
+  }
+  return true;
 }
 
 bool tsel_symbol_get_code(emacs_env *env, emacs_value obj, uint16_t *code_out) {
@@ -50,11 +66,13 @@ bool tsel_symbol_get_code(emacs_env *env, emacs_value obj, uint16_t *code_out) {
     return false;
   }
   // Extract the "code" field and make sure it's an integer
-  emacs_value Qts_symbol_code = env->intern(env, "tree-sitter-symbol-code");
-  emacs_value args[1] = { obj };
-  emacs_value code = env->funcall(env, Qts_symbol_code, 1, args);
-  uint16_t raw_code = env->extract_integer(env, code);
-  if(tsel_pending_nonlocal_exit(env)) {
+  emacs_value code;
+  if(!tsel_record_get_field(env, obj, 1, &code)) {
+    return false;
+  }
+  // Ensure the code is non-negative
+  intmax_t raw_code = env->extract_integer(env, code);
+  if(tsel_pending_nonlocal_exit(env)){
     return false;
   }
   *code_out = raw_code;
