af93fe2ca1783316126e5108796e11d1cd89edaf
Karl Otness <karl@karlotness.com>
2018-11-25 00:54:01 +0000

    Add package build for C grammar.

---
+++ langs/c/tree-sitter-lang-c.el
@@ -0,0 +1,9 @@
+;;; tree-sitter.el --- Emacs bindings to the tree-sitter parser for c  -*- lexical-binding: t; -*-
+
+;;;###autoload
+(autoload 'tree-sitter-lang-c "tree-sitter-lang-c-module"
+  "Return the tree-sitter language object for c.
+
+(fn)")
+
+(provide 'tree-sitter-lang-c)
