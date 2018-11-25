78893b4f11a91253797183ba8f673d330a6898d7
Karl Otness <karl@karlotness.com>
2018-11-25 00:53:46 +0000

    Remove explicit requirement for module file.

--- langs/python/tree-sitter-lang-python.el
+++ langs/python/tree-sitter-lang-python.el
@@ -1,7 +1,5 @@
 ;;; tree-sitter.el --- Emacs bindings to the tree-sitter parser for python  -*- lexical-binding: t; -*-
 
-(require 'tree-sitter-lang-python-module)
-
 ;;;###autoload
 (autoload 'tree-sitter-lang-python "tree-sitter-lang-python-module"
   "Return the tree-sitter language object for python.
